918e2070dd2316304383a0d1df2061d93b38a3cf
meongcode <147586074+meongcode@users.noreply.github.com>
2023-12-19 01:03:11 +0900

    Enemy Shooter Attack
    원거리 공격 구현 완성
    소환체 소환하여 소환몹까지 공격

--- Assets/Scripts/Jeong/EnemyMovement.cs
+++ Assets/Scripts/Jeong/EnemyMovement.cs
@@ -6,12 +6,16 @@ public class EnemyMovement : MonoBehaviour
     private Rigidbody2D _rigidBody2D;
     private Transform target;
     private float lastJumpTime;
+    private Animator animator; // 애니메이터 컴포넌트
+    private SpriteRenderer spriteRenderer; // 스프라이트 렌더러 컴포넌트
 
     void Start()
     {
         _rigidBody2D = GetComponent<Rigidbody2D>();
         target = GameObject.FindGameObjectWithTag("Player").transform;
         lastJumpTime = Time.time - enemyStats.jumpCooldown; // 초기화
+        animator = GetComponent<Animator>(); // 애니메이터 컴포넌트 가져오기
+        spriteRenderer = GetComponent<SpriteRenderer>(); // 스프라이트 렌더러 컴포넌트 가져오기
     }
 
     void Update()
@@ -23,24 +27,41 @@ public class EnemyMovement : MonoBehaviour
             if (distanceToTarget < enemyStats.detectionRange)
             {
                 MoveTowardsTarget();
+                animator.SetBool("isMoving", true); // 움직임 시작 시 isMoving을 true로 설정
 
                 if (target.position.y > transform.position.y + 1 && Time.time > lastJumpTime + enemyStats.jumpCooldown)
                 {
                     Jump();
                 }
             }
+            else
+            {
+                animator.SetBool("isMoving", false); // 움직임이 없을 때 isMoving을 false로 설정
+            }
+
+            // 플레이어를 향해 반전
+            FlipSpriteDirection(target.position.x > transform.position.x);
         }
     }
 
     void MoveTowardsTarget()
     {
         Vector2 direction = (target.position - transform.position).normalized;
-        _rigidBody2D.velocity = new Vector2(direction.x * enemyStats.moveSpeed, _rigidBody2D.velocity.y); // 이동
+        _rigidBody2D.velocity = new Vector2(direction.x * enemyStats.moveSpeed, _rigidBody2D.velocity.y);
     }
 
     void Jump()
     {
-        _rigidBody2D.AddForce(Vector2.up * enemyStats.jumpForce, ForceMode2D.Impulse); // 점프
-        lastJumpTime = Time.time; // 시간 업데이트
+        _rigidBody2D.AddForce(Vector2.up * enemyStats.jumpForce, ForceMode2D.Impulse);
+        lastJumpTime = Time.time;
     }
+
+    void FlipSpriteDirection(bool isFacingRight)
+    {
+        if (isFacingRight)
+            transform.localScale = new Vector3(-1, 1, 1); // 왼쪽을 향함
+        else
+            transform.localScale = new Vector3(1, 1, 1); // 오른쪽을 향함
+    }
+
 }
