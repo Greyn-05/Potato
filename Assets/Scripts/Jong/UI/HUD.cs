de4ac6518fa57b475d6c82e844af73ec39b43dc4
[Tanha] <[julus8238@gmail.com]>
2023-12-19 15:31:27 +0900

    prefab

--- Assets/Scripts/Jong/UI/HUD.cs
+++ Assets/Scripts/Jong/UI/HUD.cs
@@ -1,41 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Build.Content;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class HUD : MonoBehaviour
 {
-    public enum InfoType { Exp, HP, Mana}
+    public enum InfoType { HP,Gold }
     public InfoType type;
 
     Text mytext;
     Slider myslider;
+    CommonStatus commonStatusInstance; // CommonStatus 인스턴스를 클래스 레벨 변수로 선언
 
     private void Awake()
     {
         mytext = GetComponent<Text>();
         myslider = GetComponent<Slider>();
+        commonStatusInstance = ScriptableObject.CreateInstance<CommonStatus>(); // CommonStatus 인스턴스 생성 및 할당
     }
 
     private void LateUpdate()
     {
-        switch (type) 
+        switch (type)
         {
-            case InfoType.Exp:       //이후 게임매니저가 생기면 주석을 풀어서 사용
-                float maxExp = 100;  //GameManager.instance.경험치;
-                float curExp = 100;  //GameManager.instance.다음레벨까지의 경험치;
-                myslider.value = curExp / maxExp;
-                break;
             case InfoType.HP:
-                float maxhp = 100;   //GameManager.instance.~~
-                float curhp = 50;   //GameManager.instance.~~
+                float maxhp = commonStatusInstance.maxHealth;
+                float curhp = commonStatusInstance.hp;
                 myslider.value = curhp / maxhp;
                 break;
-            case InfoType.Mana:
-                float maxmp = 100;
-                float curmp = 50;
-                myslider.value = curmp / maxmp;
+            case InfoType.Gold:
+                float curgold = 100;
+                mytext.text = curgold.ToString();
                 break;
         }
     }
