43c88e6051afa1c0d52a24baf3f2a8cbeab77ee9
a-ou-d <148048020+a-ou-d@users.noreply.github.com>
2023-12-19 16:45:04 +0900

    CREATEMAP SCRIPT EDITTING

--- Assets/Scripts/SEYEON/CreateMap.cs
+++ Assets/Scripts/SEYEON/CreateMap.cs
@@ -9,8 +9,16 @@ using Random = UnityEngine.Random;
 public class MapPrefab
 {
     public GameObject prefab; // 맵 프리팹
-    public float width;
-    public float height;
+    public float width; // 프리팹 가로
+    public float height; // 프리팹 세로
+}
+
+[Serializable]
+public struct PrefabData
+{
+    public Vector2 position; // 프리팹 위치
+    public float width; // 가로
+    public float height; // 세로
 }
 
 public class CreateMap : MonoBehaviour
@@ -24,6 +32,8 @@ public class CreateMap : MonoBehaviour
     public float minDistanceBetweenPrefabs; // 프리팹끼리 최소 거리
     public float minHeightDifference; // 프리팹끼리 최소 높이 차이
 
+    private List<PrefabData> createdPrefabsData = new List<PrefabData>(); // 프리팹 데이터 저장
+
 
     private void Start()
     {
@@ -32,28 +42,46 @@ public class CreateMap : MonoBehaviour
 
     void GenerateMap()
     {
-        Vector2 currentPosition = startPosition; // 현재 위치를 시작 위치로
-        float lastPrefabWidth = 0; // 마지막 프리팹 가로 저장
-        float lastPrefabHeight = 0; // 마지막 프리팹 세로 저장
+        int maxPrefabCount = 20;
+        int createdPrefabCount = 0;
 
-        while (currentPosition.x < startPosition.x + mapLength) // 현재 위치의 x값이 시작 위치의 x값 + 맵 길이보다 작을 때
+        while (createdPrefabCount < maxPrefabCount) // 최대 프리팹 갯수까지
         {
             MapPrefab mapPrefabData = mapPrefabs[Random.Range(0, mapPrefabs.Length)]; // 프리팹 배열에서 프리팹 랜덤 선택
             float prefabWidth = mapPrefabData.width; // 입력한 길이 가져옴
             float prefabHeight = mapPrefabData.height; // 입력한 높이 가져옴
 
-            float nextXPosition = currentPosition.x + lastPrefabWidth / 2 + prefabWidth / 2 + Random.Range(minDistanceBetweenPrefabs, maxJumpWidth); // 다음 프리팹 위치 계산
-            float nextYPosition = currentPosition.y + lastPrefabHeight / 2 + prefabHeight / 2 + Random.Range(-maxJumpHeight, maxJumpHeight); // 다음 프리팹 위치 계산
+            Vector2 nextPosition; // 다음 프리팹 생성 위치
+            bool positionIsValid; // 위치 유효한지
 
-            nextYPosition = Mathf.Clamp(nextYPosition, startPosition.y, startPosition.y + mapHeight - prefabHeight); // // 세로 위치가 맵 범위를 넘지 않게
+            do
+            {
+                positionIsValid = true;
+                float nextXPosition = startPosition.x + Random.Range(0, mapLength - prefabWidth); // x는 맵 길이 안에서 랜덤
+                float nextYPosition = startPosition.y + Random.Range(0, mapHeight - prefabHeight); // y는 맵 높이 안에서 랜덤
 
-            Vector2 nextPosition = new Vector2(nextXPosition, nextYPosition); // 다음 프리팹의 위치 결정
+                nextPosition = new Vector2(nextXPosition, nextYPosition);
 
-            Instantiate(mapPrefabData.prefab, new Vector3(nextPosition.x, nextPosition.y, 0), Quaternion.identity); // 프리팹 생성
+                foreach (var prefabData in createdPrefabsData) // 생성된 프리팹들 위치랑 겹치는지 아닌지 
+                {
+                    float horizontalDistance = Mathf.Abs(prefabData.position.x - nextPosition.x) - (prefabData.width / 2 + prefabWidth / 2);
+                    float verticalDistance = Mathf.Abs(prefabData.position.y - nextPosition.y) - (prefabData.height / 2 + prefabHeight / 2);
 
-            currentPosition = nextPosition; // 현재 위치 업데이트
-            lastPrefabWidth = prefabWidth; // 마지막 프리팹 가로 업데이트
-            lastPrefabHeight = prefabHeight; // 마지막 프리팹 세로 업데이트
+                    if (horizontalDistance < minDistanceBetweenPrefabs && verticalDistance < minHeightDifference)
+                    {
+                        positionIsValid = false; // 겹치면 false
+                        break;
+                    }
+                }
+            }
+            while (!positionIsValid);
+
+            if (positionIsValid)
+            {
+                Instantiate(mapPrefabData.prefab, new Vector3(nextPosition.x, nextPosition.y, 0), Quaternion.identity); // 프리팹 생성
+                createdPrefabsData.Add(new PrefabData { position = nextPosition, width = prefabWidth, height = prefabHeight }); // 생성된 프리팹 위치, 길이 저장
+                createdPrefabCount++; // 프리팹 수 ++
+            }
         }
     }
 }
